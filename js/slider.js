90e19efee5c6ac54764abc5ce60371b51b6a8717
Zhukov Vadim <job3507@yandex.ru>
2014-11-12 17:24:20 +0300

    master

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+(function($){
+    jQuery.fn.pluginNae = function(options){
+        var options = $.extend({
+            leftBtn: 'leftBtn',
+            rightBtn: 'rightBtn',
+            quantly: 4,
+            autoPlay: true,
+            autoPlayDelay: 10,
+
+        })
+    }
+})
